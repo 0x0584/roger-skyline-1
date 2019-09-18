8285a68a217959cd283326d0f8b7290cce6b6982
root <root@1337>
2019-09-18 12:04:22 -0400

    update firewall hitcount

--- static-ip.sh
+++ static-ip.sh
@@ -8,8 +8,11 @@ LOCAL_IP=$(ifconfig $LOCAL_IF | grep 'inet ' | awk '{print $2}')
 LOCAL_GATEWAY=$(traceroute 8.8.8.8 | head -3 | \
 					grep 'traceroute' -A 1 | grep -v 'traceroute' \
 					| awk '{print $2}')
+ifdown $LOCAL_IF
 
 sed -i "/iface $LOCAL_IF/ s/dhcp/static/" $INTERFACES
 echo "address $LOCAL_IP" >> $INTERFACES
 echo "netmask $NETMASK" >> $INTERFACES
 echo "gateway $LOCAL_GATEWAY" >> $INTERFACES
+
+ifup $LOCAL_IF
